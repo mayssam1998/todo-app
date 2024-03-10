7fcec5dfd0541b98aa6e87109e837a6b961fc2f8
jrammouz <julieannrammouz@gmail.com>
2024-03-10 15:38:57 +0200

    refactor addtodo component

--- angular/src/app/components/addtodo/addtodo.component.ts
+++ angular/src/app/components/addtodo/addtodo.component.ts
@@ -10,28 +10,17 @@ import { TodosService } from '../../services/todos.service';
   templateUrl: './addtodo.component.html',
 })
 export class AddtodoComponent {
-  isOpen: boolean = false;
   title: string = '';
   content: string = '';
   todoService = inject(TodosService);
 
-  onFocus(): void {
-    this.isOpen = true;
-  }
-
   addToDo(): void {
     if (this.title && this.content) {
-      this.isOpen = false;
       const todo = { title: this.title, content: this.content };
       this.todoService.postToDo(todo.content).subscribe(() => {
         this.todoService.addTodo(todo);
       });
       (this.title = ''), (this.content = '');
     }
-  }
-
-  setContent(event: Event) {
-    const newContent = (event.target as HTMLDivElement).innerText;
-    this.content = newContent;
   }
 }
